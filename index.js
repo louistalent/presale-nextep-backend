08dcf29ff1db3749444eb049b2c08683d408e8ab
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2022-09-28 17:33:17 -0600

    add: front

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
+import * as path from 'path';
+
+
 var privateKey = fs.readFileSync('./certs/presale.key', 'utf8');
 var certificate = fs.readFileSync('./certs/presale.crt', 'utf8');
 
 var credentials = { key: privateKey, cert: certificate };
-
+const FRONTENDPATH = path.normalize(__dirname + './build')
+app.use(express.static(FRONTENDPATH))
 // db connect
 require('./DB/mysql');
 
@@ -28,6 +32,12 @@ app.use(express.json());
 app.set('view engine', 'html');
 //Routes
 app.use('/', require('./routes/router'));
+
+app.get('/', (req, res) => {
+    const filename = FRONTENDPATH + '/index.html'
+    res.sendFile(filename);
+})
+
 const PORT = process.env.PORT || 8443;
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(PORT, console.log("Server has started at port " + PORT));
