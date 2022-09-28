0b06c04ead863e48d37544e682a45aaf3edb31fc
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2022-09-28 13:44:49 -0600

    fix: server port

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use(express.json());
 app.set('view engine', 'html');
 //Routes
 app.use('/', require('./routes/router'));
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server has started at port " + PORT))
